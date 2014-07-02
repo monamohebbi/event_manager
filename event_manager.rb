8549b702d0398418c523d28b2ce11f43c34fddae
Mona Mohebbi <monamohebbi9211@gmail.com>
2014-07-02 19:18:19 -0400

    Changing indentation to spaces.

--- event_manager.rb
+++ event_manager.rb
@@ -3,17 +3,17 @@ require "csv"
 contents=CSV.open("event_attendees.csv",{:headers=>true, :header_converters=> :symbol})
 
 def clean_zipcode(zip)
-	if zip.nil?
-		"00000"
-	elsif zip.length<5
-		zip.rjust(5,"0")
-	else
-		zip[0...5]
-	end
+  if zip.nil?
+    "00000"
+  elsif zip.length<5
+    zip.rjust(5,"0")
+  else
+    zip[0...5]
+  end
 end
 
 counter=0
 contents.each do |row|
-	puts "#{row[:first_name]} #{clean_zipcode(row[:zipcode])}"
+  puts "#{row[:first_name]} #{clean_zipcode(row[:zipcode])}"
 end
 
